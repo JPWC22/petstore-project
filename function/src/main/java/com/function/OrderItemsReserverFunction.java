f1a995c5ed4edc5f998c4308e503affa304b366c
Johann Poon <johann_poon@epam.com>
2023-10-10 13:37:37 +0800

    update function

--- function/src/main/java/com/function/OrderItemsReserverFunction.java
+++ function/src/main/java/com/function/OrderItemsReserverFunction.java
@@ -2,12 +2,7 @@ package com.function;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.URI;
 import java.util.Map;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
@@ -32,67 +27,25 @@ public class OrderItemsReserverFunction {
             context.getLogger().warning("Failed to parse order request JSON");
             return;
         }
-
-        String connectionString = System.getenv("BlobStorageConnectionString");
-        String containerName = "order-requests";
-        String sessionId = String.valueOf(requestBody.get("id"));
-        String blobName = sessionId + ".json";
-
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString)
-                .buildClient();
-        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
-        BlobClient blobClient = containerClient.getBlobClient(blobName);
-
-        byte[] data = orderRequestJson.getBytes();
-
-        int retries = 3;
-        boolean uploadSuccess = false;
-
-        while (retries > 0) {
-            try {
-                try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data)) {
-                    blobClient.upload(inputStream, data.length, true);
-                }
-                context.getLogger().info("Order request uploaded to Blob Storage.");
-                uploadSuccess = true;
-                break;
-            } catch (IOException | BlobStorageException a) {
-                context.getLogger().warning("Error uploading order request to blob storage");
-                retries--;
-            }
-        }
-
-        if (!uploadSuccess) {
-            triggerLogicApp(orderRequestJson, context);
-        }
-    }
-
-    private void triggerLogicApp(String orderRequestJson, ExecutionContext context) {
-
-        String logicAppUrl = System.getenv("LogicAppUrl");
-
-        HttpClient httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .connectTimeout(Duration.ofSeconds(20))
-                .build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(logicAppUrl))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(orderRequestJson))
-                .build();
-
-        HttpResponse response;
         try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response != null && response.statusCode() == 202) {
-                context.getLogger().info("Successfully triggered the Logic App");
-            } else {
-                context.getLogger().warning("Failed to trigger the Logic App");
+            String connectionString = System.getenv("BlobStorageConnectionString");
+            String containerName = "order-requests";
+            String sessionId = String.valueOf(requestBody.get("id"));
+            String blobName = sessionId + ".json";
+
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString)
+                    .buildClient();
+            BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+            BlobClient blobClient = containerClient.getBlobClient(blobName);
+
+            byte[] data = orderRequestJson.getBytes();
+
+            try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data)) {
+                blobClient.upload(inputStream, data.length, true);
             }
-        } catch (Exception e) {
-            context.getLogger().warning("Error triggering the Logic App: " + e.getMessage());
+            context.getLogger().info("Order request uploaded to Blob Storage.");
+        } catch (IOException | BlobStorageException e) {
+            context.getLogger().warning("Error uploading order request to blob storage");
         }
     }
-
 }
