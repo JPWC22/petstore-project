a68d8055a9937f6b9ed6b51858c0b30433a2155c
Johann Poon <johann_poon@epam.com>
2023-10-10 11:31:12 +0800

    update service bus and function app

--- petstoreorderservice/src/main/java/com/chtrembl/petstore/order/api/ServiceBusHandler.java
+++ petstoreorderservice/src/main/java/com/chtrembl/petstore/order/api/ServiceBusHandler.java
@@ -8,8 +8,7 @@ import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 @Service
 public class ServiceBusHandler {
-    // private final String connectionString = System.getenv("ServiceBusConnectionString");
-    private final String connectionString = "Endpoint=sb://petstore901.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=LpQc07QVqy69qTMPGLjOwRfgFepzdC6Ao+ASbOTtlE4=";
+    private final String connectionString = System.getenv("ServiceBusConnectionString");
     private final String queueName = "petstore_order";
         
     public void sendMessage(String message) {
